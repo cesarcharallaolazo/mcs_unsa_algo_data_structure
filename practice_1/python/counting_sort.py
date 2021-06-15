6e8efdf124525d665aaee7fafc6a59be6a09795a
Cesar Charalla <cesar.charalla@rappi.com>
2021-06-14 20:28:52 -0500

    counting sort

--- practice_1/python/counting_sort.py
+++ practice_1/python/counting_sort.py
@@ -1,6 +1,32 @@
 # counting sort algorithm
+import pdb
 
-def count_sort(my_list = None):
-    pass
+def count_sort(my_list=None):
+    c = []
+    # b = [0 for i in range(len(my_list))]
+    for _ in range(max(my_list) + 1):
+        c.append(0)
+
+    for i in range(len(my_list)):
+        c[my_list[i]] = c[my_list[i]] + 1
+
+    # for j in range(1, len(my_list) + 1):
+    #     c[my_list[j]] = c[my_list[j]] + 1
+
+    for i in range(1, max(my_list)+1):
+        c[i] = c[i] + c[i - 1]
+
+    b = [0 for i in range(len(my_list))]
+    for i in range(len(my_list)):
+        jp = len(my_list)-1 - i
+        c[my_list[jp]] = c[my_list[jp]] - 1
+        b[c[my_list[jp]]] = my_list[jp]
+
+    return b
+
 
 # evaluate
+
+ll = [3, 1, 8, 9, 11 , 8 , 343]
+print(max(ll))
+print(count_sort(ll))
