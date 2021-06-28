7dc566fdd9d9ba60df00e4659e05dfd5dc923986
JPOMM <jpommaqp@gmail.com>
2021-06-28 17:25:53 -0500

    .

--- practice_1/c++/main.cpp
+++ practice_1/c++/main.cpp
@@ -39,6 +39,7 @@ void callOfstreamInsertionSort();
 void callOfstreamMergeSort();
 void callOfstreamQuicksort();
 void callOfstreamSelectionSort();
+void callpila();
 
 void readTotalFiles(){
 	sizeFile100=readFiles(100);
@@ -80,6 +81,7 @@ void menuAlgorithm(){
     printf("5)mergeSort\n");
     printf("6)quickSort\n");
     printf("7)selectionSort\n");
+    printf("8)stack\n");
     printf("=================================\n");
     scanf("%d",&opc);
     printf("=================================\n");
@@ -130,7 +132,85 @@ void menuAlgorithm(){
 		  cout << "Error";
 		}
     break;
+	   case 8: //pila
+    	try {
+    		callpila();
+    		menuAlgorithm();
+    	}catch (...) {
+		  cout << "Error";
+		}
+    break;
     }
+}
+class Pila {
+private:
+    class Nodo {
+		public:
+			int info;
+			Nodo *sig;
+    };
+    Nodo *raiz;
+public:
+    Pila();
+    ~Pila();
+    void insertar(int x);
+    int extraer();
+    void imprimir();
+};
+Pila::Pila(){raiz = NULL;}
+void Pila::insertar(int x){
+    Nodo *nuevo;
+    nuevo = new Nodo();
+    nuevo->info = x;
+    if (raiz == NULL){
+        raiz = nuevo;
+        nuevo->sig = NULL;
+    }else{
+        nuevo->sig = raiz;
+        raiz = nuevo;
+    }
+}
+void Pila::imprimir(){
+    Nodo *reco = raiz;
+    cout << "Listado de todos los elementos de la pila.\n";
+    while (reco != NULL){
+        cout << reco->info << "-";
+        reco = reco->sig;
+    }
+    cout << "\n";
+}
+int Pila::extraer(){
+    if (raiz != NULL){
+        int informacion = raiz->info;
+        Nodo *bor = raiz;
+        raiz = raiz->sig;
+        delete bor;
+        return informacion;
+    }else{
+        return -1;
+    }
+}
+Pila::~Pila(){
+    Nodo *reco = raiz;
+    Nodo *bor;
+    while (reco != NULL){
+        bor = reco;
+        reco = reco->sig;
+        delete bor;
+    }
+}
+void callpila(){
+	Pila *pila1;
+    pila1= new Pila();
+    pila1->insertar(10);
+    pila1->insertar(40);
+    pila1->insertar(3);
+	pila1->insertar(5);
+	pila1->insertar(9);
+    pila1->imprimir();
+    cout<<"Extraemos de la pila:" <<pila1->extraer()<<"\n";
+    pila1->imprimir();
+    delete pila1;
 }
 void callOfstreamBubbleSort(){
 	std::ofstream outfile ("bubble_sort.txt");
