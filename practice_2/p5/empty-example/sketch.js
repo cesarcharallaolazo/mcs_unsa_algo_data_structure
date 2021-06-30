f40e1cde6aaa35765f484e899d67224b1e98f34a
JPOMM <jpommaqp@gmail.com>
2021-06-29 22:10:33 -0500

    eliminando

--- practice_2/p5/empty-example/sketch.js
+++ practice_2/p5/empty-example/sketch.js
@@ -52,9 +52,45 @@ console.log(bst2.getDepth()); // 5
 console.log(bst2.nodeAverages()); //[ 10, 12.5, 13.67, 22, 80, 98 ]
 
 console.log(bst2.convertToLinkedList());
+
+//https://p5js.org/es/examples/structure-functions.html
+
+
+function setup(){
+createCanvas(400,400);
+background(220);
+}
+
+function draw(){
+  for(let i=0; i<6;i++){
+    fill(random(250),random(100),random(255,255));
+    ellipse(i*60+40,random(50),30);
+  }
+}
+/*
+let mic; let micLevel; 
+function setup() { 
+  createCanvas(400, 400); 
+  mic = new p5.AudioIn(); 
+  mic.start(); } 
+  
+function draw() { 
+  micLevel = mic.getLevel(); 
+  background(5); 
+  stroke(255, round(micLevel * 800), round(micLevel*255)); 
+  strokeWeight(micLevel * 200); 
+  for(let i =0; i < 6; i++) { // for loop counting from 0 to 6 
+    fill(random(250), random(100), random(255), 255); 
+    //1 circle is drawn with every loop, so 6 circles total 
+    ellipse(i*60 + 40, micLevel*5000 + random(50), 50); 
+    //micLevel for the y value caues the circles to go up and down with the volume, i*60 means a new circle is drawn every 60 pixels along the x axis 
+  }
+ }*/
+
+
 //[ 3, 5, 7, 8, 9, 10, 20, 30, 50, 80, 98 ]
 //{ head: { value: 3, next: { value: 5, next: [Object] } } }
-
+/*
 function setup() {
   createCanvas(720, 400);
   background(51);
@@ -77,4 +113,4 @@ function drawTarget(xloc, yloc, size, num) {
     fill(i * grayvalues);
     ellipse(xloc, yloc, size - i * steps, size - i * steps);
   }
-}
+}*/
