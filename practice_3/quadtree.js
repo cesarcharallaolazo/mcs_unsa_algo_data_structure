accc1cc08dbc23674de7ed5ad0cd1f7af8acb2a1
JPOMM <jpommaqp@gmail.com>
2021-07-03 13:02:56 -0500

    quadtree

--- practice_3/quadtree.js
+++ practice_3/quadtree.js
@@ -7,18 +7,155 @@ class Point {
 }    
 class Rectangle {
     constructor (x, y, w, h) {
-    this.x = x; // center
-    this.y = y;
-    this.w = w; // half width
-    this.h = h; // half height
+        this.x = x; // center
+        this.y = y; // center
+        this.w = w; // half width
+        this.h = h; // half height
     }
     // verifica si este objeto contiene un objeto Punto
     contains ( point ){
+        return (point.x >= this.x - this.w && point.x < this.x + this.w &&
+            point.y >= this.y - this.h && point.y < this.y + this.h);
     }
     // verifica si este objeto se intersecta con otro objeto Rectangle
     intersects ( range ){
+        return (range.x - range.w > this.x + this.w || range.x + range.w < this.x - this.w ||
+            range.y - range.h > this.y + this.h || range.y + range.h < this.y - this.h);
     }
 }
+
+//Edwin
+/*
+contains(point) {
+    return (point.x >= this.x - this.w && point.x < this.x + this.w &&
+      point.y >= this.y - this.h && point.y < this.y + this.h);
+  }
+  intersects(range) {
+    return (range.x - range.w > this.x + this.w || range.x + range.w < this.x - this.w ||
+      range.y - range.h > this.y + this.h || range.y + range.h < this.y - this.h);
+  }
+*/ 
+//mirrella
+/*
+contains(point) {
+    var temp = (
+        (point.x >= this.x - this.w) && (point.x < this.x + this.w) && // ancho
+        (point.y >= this.y - this.h) && (point.y < this.y + this.h) // alto
+      )
+    return temp;
+  }
+  // verifica si este objeto se intersecta con otro objeto Rectangle
+  intersects(range) { // es de tipo rectangulo 
+    var temp = (
+      (range.x - range.w > this.x + this.w) || (range.x + range.w < this.x - this.w) ||
+      (range.y - range.h > this.y + this.h )||(range.y + range.h < this.y - this.h)
+    )
+    return temp 
+  }
+*/
+//Mirella
+/*
+subdivide() {
+    let northeast_rect = new Rectangle(
+        (this.boundary.x + this.boundary.w) / 2, 
+        (this.boundary.y + this.boundary.h) / 2,
+        this.boundary.w / 2, 
+        this.boundary.h / 2);
+    let northwest_rect = new Rectangle(
+        (this.boundary.x + this.boundary.w) / 2, 
+        (this.boundary.y + this.boundary.h) / 2,
+        this.boundary.w / 2, 
+        this.boundary.h / 2);
+    let southeast_rect = new Rectangle(
+        (this.boundary.x + this.boundary.w) / 2, 
+        (this.boundary.y + this.boundary.h) / 2,
+        this.boundary.w / 2, 
+        this.boundary.h / 2);
+    let southwest_rect = new Rectangle(
+        (this.boundary.x + this.boundary.w) / 2, 
+        (this.boundary.y + this.boundary.h) / 2,
+        this.boundary.w / 2, 
+        this.boundary.h / 2);
+    this.northeast = new QuadTree(northeast_rect, this.capacity);
+    this.northwest = new QuadTree(northwest_rect, this.capacity);
+    this.southeast = new QuadTree(southeast_rect, this.capacity);
+    this.southwest = new QuadTree(southwest_rect, this.capacity);
+    this.divided = true
+  }
+*/ 
+//Edwin
+/*
+subdivide() {
+    let _x = this.boundary.x;
+    let _y = this.boundary.y;
+    let _w = this.boundary.w * 0.5;
+    let _h = this.boundary.h * 0.5;
+    let px0 = _x + _w;
+    let px1 = _x - _w;
+    let py0 = _y + _h;
+    let py1 = _y - _h;
+    let ne = new Rectangle(px0, py1, _w, _h);
+    this.northeast = new QuadTree(ne, this.capacity);
+    let nw = new Rectangle(px1, py1, _w, _h);
+    this.northwest = new QuadTree(nw, this.capacity);
+    let se = new Rectangle(px0, py0, _w, _h);
+    this.southeast = new QuadTree(se, this.capacity);
+    let sw = new Rectangle(px1, py0, _w, _h);
+    this.southwest = new QuadTree(sw, this.capacity);
+    this.divided = true;
+  }
+*/
+
+//Mirella
+/**
+ insert(point) {
+    if (!this.boundary.contains(point)) {
+      return false;
+    }
+    if ( this.points.length < this.capacity ) {
+      this.points.push(point)
+      return true
+    } else {
+      if (this.subdivide === false) {
+        this.subdivide()
+      }
+      return (
+          this.northeast.insert(point) ||
+          this.northwest.insert(point) ||
+          this.southeast.insert(point) ||
+          this.southwest.insert(point) 
+        )
+    }
+  }
+
+ */
+
+//Edwin
+/*
+insert(point) {
+    if (!this.boundary.contains(point)) {
+      return false;
+    }
+    if (this.points.length < this.capacity) {
+      this.points.push(point);
+      return true;
+    } else {
+      if (!this.divided) {
+        this.subdivide();
+      }
+      if (this.northeast.insert(point)) {
+        return true;
+      } else if (this.northwest.insert(point)) {
+        return true;
+      } else if (this.southeast.insert(point)) {
+        return true;
+      } else if (this.southwest.insert(point)) {
+        return true;
+      }
+    }
+  }
+*/
+
 class QuadTree {
      constructor ( boundary , n){
      this.boundary = boundary; // Rectangle
@@ -27,15 +164,27 @@ class QuadTree {
      this.divided = false;
      }
      // divide el quadtree en 4 quadtrees
-     subdivide () {
+    subdivide () { // crea 4 nuevos nodos
         // Algoritmo
         // 1: Crear 4 hijos: qt_northeast , qt_northwest , qt_southeast , qt_southwest
         // 2: Asignar los QuadTree creados a cada hijo
-        this.northeast = qt_northeast;
-        this.northwest = qt_northwest;
-        this.southeast = qt_southeast;
-        this.southwest = qt_southwest;
-
+        let _x = this.boundary.x;
+        let _y = this.boundary.y;
+        let _w = this.boundary.w * 0.5;
+        let _h = this.boundary.h * 0.5;
+        let px0 = _x + _w;
+        let px1 = _x - _w;
+        let py0 = _y + _h;
+        let py1 = _y - _h;
+        let ne = new Rectangle(px0, py1, _w, _h);
+        this.northeast = new QuadTree(ne, this.capacity);
+        let nw = new Rectangle(px1, py1, _w, _h);
+        this.northwest = new QuadTree(nw, this.capacity);
+        let se = new Rectangle(px0, py0, _w, _h);
+        this.southeast = new QuadTree(se, this.capacity);
+        let sw = new Rectangle(px1, py0, _w, _h);
+        this.southwest = new QuadTree(sw, this.capacity);
+        this.divided = true;
         // 3.- Hacer: this.divided <- true
     }
     insert ( point ){
@@ -50,19 +199,38 @@ class QuadTree {
         // this.northwest.insert ( point );
         // this.southeast.insert ( point );
         // this.southwest.insert ( point );
+        if (!this.boundary.contains(point)) {
+            return false;
+          }
+          if (this.points.length < this.capacity) {
+            this.points.push(point);
+            return true;
+          } else {
+            if (!this.divided) {
+              this.subdivide();
+            }
+            if (this.northeast.insert(point)) {
+              return true;
+            } else if (this.northwest.insert(point)) {
+              return true;
+            } else if (this.southeast.insert(point)) {
+              return true;
+            } else if (this.southwest.insert(point)) {
+              return true;
+            }
+          }
     }
-
     show () {
-        stroke (255) ;
-        strokeWeight (1) ;
-        noFill () ;
-        rectMode ( CENTER );
-        rect ( this.boundary.x , this.boundary.y , this.boundary.w *2 , this.boundary.h*2) ;
-        if( this.divided ) {
-        this.northeast.show () ;
-        this.northwest.show () ;
-        this.southeast.show () ;
-        this.southwest.show () ;
+            stroke (255) ;
+            strokeWeight (1) ;
+            noFill () ;
+            rectMode ( CENTER );
+            rect ( this.boundary.x , this.boundary.y , this.boundary.w *2 , this.boundary.h*2) ;
+            if( this.divided ) {
+            this.northeast.show () ;
+            this.northwest.show () ;
+            this.southeast.show () ;
+            this.southwest.show () ;
         }
         for (let p of this.points ){
         strokeWeight (4) ;
--- practice_3/sketch.js
+++ practice_3/sketch.js
@@ -3,12 +3,18 @@ let count = 0;
 
 function setup () {
     createCanvas (400 ,400) ;
+    //let rect1= new Rectangle(4,4,8,8);
+    //let rect2= new Rectangle(5,5,7,7);
+    //let point= new Point(3,3)
+    //console.log("Intersectan? : "+ rect1.intersects(rect2))
+    //console.log("Contiene el punto: "+rect1.contains(point));
+    //let vector=[]
     // centre point and half of width and height
     let boundary = new Rectangle (200 ,200 ,200 ,200) ;
     // each leave just could have 4 elements
     qt = new QuadTree ( boundary , 4) ;
     console.log (qt);
-    for (let i=0; i < 3; i ++) {
+    for (let i=0; i < 10; i ++) {
         let p = new Point ( Math.random () * 400 , Math.random () * 400) ;
         qt.insert (p);
     }
