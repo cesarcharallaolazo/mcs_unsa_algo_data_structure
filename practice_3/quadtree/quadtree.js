899f884dff957d2b31307a43a9acb00d2a1cc472
Paul <pcahuana.27@gmail.com>
2021-07-08 23:54:31 -0500

    QuadTree- con cuadro de busqueda

--- practice_3/quadtree/quadtree.js
+++ practice_3/quadtree/quadtree.js
@@ -4,25 +4,39 @@ class Point {
     this.y = y;
     this.userData = userData;
     }
-}    
+}   
+
+
 class Rectangle {
     constructor (x, y, w, h) {
+    this.x = x; // center
+    this.y = y;
+    this.w = w; // half width
+    this.h = h; // half height
         this.x = x; // center
         this.y = y; // center
         this.w = w; // half width
         this.h = h; // half height
     }
     // verifica si este objeto contiene un objeto Punto
-    contains(point) {
-     return (point.x >= this.x - this.w && point.x < this.x + this.w &&
-      point.y >= this.y - this.h && point.y < this.y + this.h);
-  }
+    contains ( point ){
+       
+    return ((point.x >= this.x - this.w) && (point.x < this.x + this.w) && 
+      (point.y >= this.y - this.h) && (point.y < this.y + this.h));
+    }
     // verifica si este objeto se intersecta con otro objeto Rectangle
-    intersects(range) { // es de tipo rectangulo 
-    return (range.x - range.w > this.x + this.w || range.x + range.w < this.x - this.w ||
-      range.y - range.h > this.y + this.h || range.y + range.h < this.y - this.h);
-  }
+    intersects ( range ){
+
+
+        var temp = (
+      (range.x - range.w > this.x + this.w) || (range.x + range.w < this.x - this.w) ||
+      (range.y - range.h > this.y + this.h )||(range.y + range.h < this.y - this.h)
+    )
+    return temp ;
+    }
 }
+
+
 class QuadTree {
      constructor ( boundary , n){
      this.boundary = boundary; // Rectangle
@@ -32,23 +46,27 @@ class QuadTree {
      }
      // divide el quadtree en 4 quadtrees
     subdivide () { // crea 4 nuevos nodos
+        // Algoritmo
+        // 1: Crear 4 hijos: qt_northeast , qt_northwest , qt_southeast , qt_southwest
+        // 2: Asignar los QuadTree creados a cada hijo
         let _x = this.boundary.x;
-    let _y = this.boundary.y;
-    let _w = this.boundary.w * 0.5;
-    let _h = this.boundary.h * 0.5;
-    let px0 = _x + _w;
-    let px1 = _x - _w;
-    let py0 = _y + _h;
-    let py1 = _y - _h;
-    let ne = new Rectangle(px0, py1, _w, _h);
-    this.northeast = new QuadTree(ne, this.capacity);
-    let nw = new Rectangle(px1, py1, _w, _h);
-    this.northwest = new QuadTree(nw, this.capacity);
-    let se = new Rectangle(px0, py0, _w, _h);
-    this.southeast = new QuadTree(se, this.capacity);
-    let sw = new Rectangle(px1, py0, _w, _h);
-    this.southwest = new QuadTree(sw, this.capacity);
-    this.divided = true;
+        let _y = this.boundary.y;
+        let _w = this.boundary.w * 0.5;
+        let _h = this.boundary.h * 0.5;
+        let px0 = _x + _w;
+        let px1 = _x - _w;
+        let py0 = _y + _h;
+        let py1 = _y - _h;
+        let ne = new Rectangle(px0, py1, _w, _h);
+        this.northeast = new QuadTree(ne, this.capacity);
+        let nw = new Rectangle(px1, py1, _w, _h);
+        this.northwest = new QuadTree(nw, this.capacity);
+        let se = new Rectangle(px0, py0, _w, _h);
+        this.southeast = new QuadTree(se, this.capacity);
+        let sw = new Rectangle(px1, py0, _w, _h);
+        this.southwest = new QuadTree(sw, this.capacity);
+        this.divided = true;
+        // 3.- Hacer: this.divided <- true
     }
     insert ( point ){
         // Algoritmo
@@ -63,26 +81,25 @@ class QuadTree {
         // this.southeast.insert ( point );
         // this.southwest.insert ( point );
         if (!this.boundary.contains(point)) {
-      return false;
-    }
-    if (this.points.length < this.capacity) {
-      this.points.push(point);
-      return true;
-    } else {
-      if (!this.divided) {
-        this.subdivide();
-      }
-      if (this.northeast.insert(point)) {
-        return true;
-      } else if (this.northwest.insert(point)) {
-        return true;
-      } else if (this.southeast.insert(point)) {
-        return true;
-      } else if (this.southwest.insert(point)) {
-        return true;
-      }
-    }
-  
+            return false;
+          }
+          if (this.points.length < this.capacity) {
+            this.points.push(point);
+            return true;
+          } else {
+            if (!this.divided) {
+              this.subdivide();
+            }
+            if (this.northeast.insert(point)) {
+              return true;
+            } else if (this.northwest.insert(point)) {
+              return true;
+            } else if (this.southeast.insert(point)) {
+              return true;
+            } else if (this.southwest.insert(point)) {
+              return true;
+            }
+          }
     }
     show () {
             stroke (255) ;
@@ -99,7 +116,6 @@ class QuadTree {
         for (let p of this.points ){
         strokeWeight (4) ;
         point (p.x , p.y );
-        
         }
     }
 }
