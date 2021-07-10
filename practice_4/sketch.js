f3ebb852ca8b4d52bb9270852b8a6ac4b7ffb605
JPOMM <jpommaqp@gmail.com>
2021-07-10 13:12:27 -0500

    kdtree

--- practice_4/sketch.js
+++ practice_4/sketch.js
@@ -5,11 +5,11 @@ function setup(){
     background(0);
     for (var x = 0; x < width; x += width / 10) {
         for (var y = 0; y < height; y += height / 5) {
-                stroke(125, 125, 125); 
-                strokeWeight(1); 
-                line(x,0,x,height);
-                line(0,y,width,y);
-            }
+            stroke(125, 125, 125); 
+            strokeWeight(1); 
+            line(x,0,x,height);
+            line(0,y,width,y);
+        }
     }
     var data = [];
     for(let i=0; i<12;i++){
@@ -17,10 +17,13 @@ function setup(){
         var y = Math.floor(Math.random() * height); 
         data.push([x,y]);
         fill(255, 255, 255);
-        circle(x, height - y, 7); //200-y para q se dibuje apropiadamente 
+        //circle(x,height-y,7); //200-y para q se dibuje apropiadamente 
         textSize(8);
         text(x + ',' + y, x + 5, height - y);//200-y para q se dibuje aproximadamente        
     }
+    //console.log(data)
     var root = build_kdtree(data);
     console.log(root); 
+    console.log("digraph G { \n" + generate_dot(root)+ "}");
+    console.log("Altura del arbol: "+getHeight(root));
 }
